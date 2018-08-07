5172d77ab3f7f11fb349fd4a148435dfc24740fb
PC_08 <PC_08@DESKTOP-N7Q3SJN>
2018-08-07 11:02:26 -0500

    Se agrega clase principal

---
+++ Areas/src/mx/edu/itlapiedad/Areas.java
@@ -0,0 +1,13 @@
+package mx.edu.itlapiedad;
+
+public class Areas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Triangulo t = new Triangulo(10,20);
+		System.out.println(t.getArea());
+		
+	}
+
+}
